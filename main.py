fff1192813018377e696ebced37b697ff644c5fc
Youssef <youssefislam2682003@gmail.com>
2024-11-03 20:06:02 +0100

    ..

--- main.py
+++ main.py
@@ -1,27 +1,2 @@
 print("hello")
 
-print("I am andrew dweri")
-print("i am youssef islam")
-
-
-
-
-
-
-
-
-print(" Hi egypt")
-print("test")
-
-
-
-
-print("code")
-
-print(" Hi egypt")
-
-print("hi i am kenzy")
-
-print("test code")
-
-print("test git repository")
