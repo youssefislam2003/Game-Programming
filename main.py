3969694f680434f4d92857a7f951be99b0ef1269
Youssef <youssefislam2682003@gmail.com>
2024-12-20 01:26:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ gravity, bird_velocity = 0.5, 0
 bird_x, bird_y = 100, WIN_H // 2
 bird_w, bird_h = 80, 80
 pipes = [{'x': WIN_W, 'h': rnd.randint(300, 500), 'gap': 300}]
+score, high_score, game_over= 0, 0, False
+
+win = pg.display.set_mode((WIN_W, WIN_H))
+pg.display.set_caption("Pharonic Flap")
+
+SCRIPT_DIR = os.path.dirname(os.path.dirname(__file__))
+ASSETS_DIR = os.path.join(SCRIPT_DIR, 'assets')
