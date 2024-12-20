8ddd0f2799769ef8a9ec5126d455cdd77b9cfbd2
Youssef <youssefislam2682003@gmail.com>
2024-12-20 01:18:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import pygame as pg
 import random as rnd 
 import logging as lg
 lg.basicConfig(level=lg.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+pg.init()
+pg.mixer.init()
+
+WIN_W, WIN_H = 800, 800
+FPS = 60
+COLOR_BG, COLOR_BIRD, COLOR_PIPE, COLOR_TEXT, COLOR_BTN = (255, 255, 255), (255, 0, 0), (34, 139, 34), (0, 0, 0), (200, 200, 200)
+
+gravity, bird_velocity = 0.5, 0 
